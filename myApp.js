01e58c3d5ecd75b55b20b418da69ba1311732bc4
Victor Camacho <victor.camacho@block.one>
2021-04-01 12:31:03 -0400

    model.find() example

--- myApp.js
+++ myApp.js
@@ -37,7 +37,10 @@ var createManyPeople = function(arrayOfPeople, done) {
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({name: personName}, function(err,data){
+    if (err) return console.log(err);
+    done(null , data);
+  });      
 };
 
 const findOneByFood = (food, done) => {
