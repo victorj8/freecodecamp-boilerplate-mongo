dc484b2f8168fceb7759cbbb92508999c93afded
Victor Camacho <victor.camacho@block.one>
2021-04-03 16:00:26 -0400

    findbyidandremove excercise

--- myApp.js
+++ myApp.js
@@ -83,7 +83,10 @@ const findAndUpdate = (personName, done) => {
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove(personId, (err, data) => {
+    if(err) return console.log(err);
+    done(null , data);
+  })
 };
 
 const removeManyPeople = (done) => {
